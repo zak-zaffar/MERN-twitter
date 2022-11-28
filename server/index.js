6cb8b6eb05e20b96288b1d7a3227052294850093
zak-zaffar <zakzaffar@gmail.com>
2022-11-28 22:48:08 +0000

    Authorization

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 // CONFIG
 
@@ -35,6 +37,12 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({ storage })
+
+// Route with file
+app.post("/auth/register", upload.single("picture"), register);
+
+// Routes
+app.use("/auth", authRoutes);
 
 //Mongoose setup
 const PORT = process.env.PORT || 6001;
