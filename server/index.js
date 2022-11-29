8fa9e148ea4cef9353ffc0edd69f10e9c9d7d24f
zak-zaffar <zakzaffar@gmail.com>
2022-11-29 02:42:32 +0000

    post routes setup

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/post.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // CONFIG
 
@@ -41,10 +44,12 @@ const upload = multer({ storage })
 
 // Route with file
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture") , createPost)
 
 // Routes
 app.use("/auth", authRoutes);
-app.use("/users", usersRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 //Mongoose setup
 const PORT = process.env.PORT || 6001;
