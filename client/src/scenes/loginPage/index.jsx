482c374878b56fb48c2e1d22585c2a88ccbc924c
zak-zaffar <zakzaffar@gmail.com>
2022-11-30 23:44:07 +0000

    navbar

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,24 @@
+import { Box, Typography, useTheme, useMediaQuery} from "@mui/material";
 
 const LoginPage = () => {
-    return (<div>LoginPage</div>)
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    return (
+        <Box>
+            <Box
+                width="100%"
+                backgroundColor={theme.palette.background.alt}
+                p="1rem 6%"
+                textAlign="center"
+            >
+                <Typography fontWeight="bold" fontSize="32px" color="primary">
+                    TwitBook
+                </Typography>
+            </Box>
+        </Box>
+    )
+        
 }
 
 export default LoginPage
