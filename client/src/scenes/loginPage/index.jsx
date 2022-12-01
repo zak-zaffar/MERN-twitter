b886f70571e0b5ea1ed9a72cbb41be0af1b98861
zak-zaffar <zakzaffar@gmail.com>
2022-12-01 17:15:01 +0000

    login

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,4 +1,5 @@
 import { Box, Typography, useTheme, useMediaQuery} from "@mui/material";
+import Form from "./Form"
 
 const LoginPage = () => {
     const theme = useTheme();
@@ -15,6 +16,18 @@ const LoginPage = () => {
                 <Typography fontWeight="bold" fontSize="32px" color="primary">
                     TwitBook
                 </Typography>
+            </Box>
+            <Box
+                width={isNonMobileScreens ? "50%" : "93%"}
+                p="2rem"
+                m="2rem auto"
+                borderRadius="1.5rem"
+                backgroundColor={theme.palette.background.alt}
+            >
+                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
+                    Welcome to TwitBook, the official Book of Twit
+                </Typography>
+                <Form/>
             </Box>
         </Box>
     )
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,39 +5,40 @@ import User from '../models/User.js';
 // REGISTER USER
 
 export const register = async (req, res) => {
-    try {
-        const {
-            firstName,
-            lastname,
-            email,
-            password,
-            picturePath,
-            friends,
-            location,
-            occupation
-        } = req.body;
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
 
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
-        const newUser = new User({
-            firstName,
-            lastname,
-            email,
-            password: passwordHash,
-            picturePath,
-            friends,
-            location,
-            occupation,
-            viewedProfile: Math.floor(Math.random() * 10000),
-            impressions: Math.floor(Math.random() * 10000),
-        })
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
 
 // LOGIN USER
 
